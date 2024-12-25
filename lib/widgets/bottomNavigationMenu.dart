b14f1cff517fa1be265f87aa13a58e21cfcaaef3
danialnbl <danialnabil0208@gmail.com>
2024-12-26 02:24:35 +0800

    up bottom navbar

--- lib/widgets/bottomNavigationMenu.dart
+++ lib/widgets/bottomNavigationMenu.dart
@@ -1,39 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hcms/screens/payment/checkout.dart';
 import '../screens/user_view.dart';
 
-class BottomNavigationMenu extends StatelessWidget {
+class BottomNavigationMenu extends StatefulWidget {
   const BottomNavigationMenu({super.key});
+
+  @override
+  State<BottomNavigationMenu> createState() => _BottomNavigationMenuState();
+}
+
+class _BottomNavigationMenuState extends State<BottomNavigationMenu> {
+  int _selectedIndex = 0;
+
+// Letak Page Korang
+  final List<Widget> _pages = [
+    const UserView(),
+    const Checkout(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-          height: 80,
-          elevation: 0,
-          selectedIndex: 0,
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.home_outlined),
-              selectedIcon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.calendar_month),
-              selectedIcon: Icon(Icons.calendar_month),
-              label: 'Booking',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.insert_chart_rounded),
-              selectedIcon: Icon(Icons.insert_chart_rounded),
-              label: 'Reports',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.notifications_outlined),
-              selectedIcon: Icon(Icons.notifications),
-              label: 'Profile',
-            ),
-          ]),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Report',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
